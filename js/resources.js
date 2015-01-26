6ba4f34e47edb57fdcbf5f3b08c33161c8443555
Axel Tanner <axeltanner38@gmail.com>
2015-01-26 13:19:19 -0800

    vid 11 1st

--- js/resources.js
+++ js/resources.js
@@ -11,6 +11,8 @@ game.resources = [
 	 //program loads orc
 	 {name: "player", type:"image", src: "data/img/orcSpear.png"},
 
+	 {name: "tower", type:"image", src: "data/img/tower_round.svg.png"},
+
 	/* Atlases 
 	 * @example
 	 * {name: "example_tps", type: "tps", src: "data/img/example_tps.json"},
