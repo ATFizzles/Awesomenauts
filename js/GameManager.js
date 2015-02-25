b7a5bb65aa853befb174f53d45e308bee2812da2
Axel Tanner <axeltanner38@gmail.com>
2015-02-25 11:53:25 -0800

    vid 29 --> separated entity files

---
+++ js/GameManager.js
@@ -0,0 +1,54 @@
+//whole class that manages timers
+//not an entity, just an object
+game.GameManager = Object.extend({
+	//uses same functions as usual
+	//constructor function
+	init: function(x, y, settings){
+		//setting key variables
+		this.now = new Date().getTime();
+		//keeps track of the last creep that spawned
+		this.lastCreep = new Date().getTime();
+		//new paused function
+		this.paused = false;
+		//keeps program updating
+		this.alwaysUpdate = true;
+	},
+	//updates at the end
+	update: function(){
+		//keeps track of timer
+		this.now = new Date().getTime();
+
+		//if player dies...
+		if(game.data.player.dead){
+			//removes character from the map
+			me.game.world.removeChild(game.data.player);
+			//respawns player at (10, 0)
+			me.state.current().resetPlayer(10, 0);
+		}
+
+
+		//keeps track of the amount of gold you get per creep
+		//rounds to 20 on one second interval
+		//makes sure creeps dont spawn within a second
+		if(Math.round(this.now/1000)%20 ===0 && (this.now - this.lastCreep >= 1000)){
+			//gives one gold per creep kill
+			game.data.gold += 1;
+			console.log("Current gold: " + game.data.gold);
+		}
+
+
+		//keeps track of if it needs to make a new creep
+		//rounds to 10 on one second interval
+		//makes sure creeps dont spawn within a second
+		if(Math.round(this.now/1000)%10 ===0 && (this.now - this.lastCreep >= 1000)){
+			//updates timer to this.now
+			this.lastCreep = this.now;
+			//builds creep
+			var creepe = me.pool.pull("EnemyCreep", 1000, 0, {});
+			//adds creep into the world
+			me.game.world.addChild(creepe, 5);
+		}
+
+		return true;
+	}
+});
