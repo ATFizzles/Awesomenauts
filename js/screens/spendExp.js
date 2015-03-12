7370c65d16f5faded5620ad0e958ee732996eed2
Axel Tanner <axeltanner38@gmail.com>
2015-03-11 22:09:27 -0700

    vid 44 comments

--- js/screens/spendExp.js
+++ js/screens/spendExp.js
@@ -9,7 +9,7 @@ game.SpendExp = me.ScreenObject.extend({
 		//-10 puts exp screen in back in z layer
 		me.game.world.addChild(new me.Sprite(0, 0, me.loader.getImage("exp-screen")), -10);
 
-
+		//registers f1-f5 keys during exp screen
 		me.input.bindKey(me.input.KEY.F1, "F1");
 		me.input.bindKey(me.input.KEY.F2, "F2");
 		me.input.bindKey(me.input.KEY.F3, "F3");
@@ -36,7 +36,7 @@ game.SpendExp = me.ScreenObject.extend({
 				this.font.draw(renderer.getContext(), "PRESS F1-F4 TO BUY, F5 TO SKIP", this.pos.x, this.pos.y);
 				this.font.draw(renderer.getContext(), "CURRENT EXP: " + game.data.exp.toString(), this.pos.x + 100, this.pos.y + 50);
 				//adding spending options with new coords
-				this.font.draw(renderer.getContext(), "F1: INCREASE GOLD PRODUCTION: CURRENT LEVEL: " + game.data.exp1.toString() + " COST: " + ((game.data.exp1 + 1) * 10), this.pos.x, this.pos.y + 100);
+				this.font.draw(renderer.getContext(), "F1: INCREASE GOLD PRODUCTION    CURRENT LEVEL: " + game.data.exp1.toString() + " COST: " + ((game.data.exp1 + 1) * 10), this.pos.x, this.pos.y + 100);
 				this.font.draw(renderer.getContext(), "F2: ADD STARTING GOLD " + game.data.exp.toString(), this.pos.x, this.pos.y + 150);
 				this.font.draw(renderer.getContext(), "F3: INCREASE ATTACK DAMAGE " + game.data.exp.toString(), this.pos.x, this.pos.y + 200);
 				this.font.draw(renderer.getContext(), "F4: INCREASE STARTING HEALTH " + game.data.exp.toString(), this.pos.x, this.pos.y + 250);
@@ -44,7 +44,11 @@ game.SpendExp = me.ScreenObject.extend({
 
 		})));
 
+		//building event handler
+		//when a key is pressed
 		this.handler = me.event.subscribe(me.event.KEYDOWN, function (action, keyCode, edge){
+			//checks all buttons f1-f5
+			//determines which one is being picked
 			if(action === "F1"){
 
 			}
@@ -58,6 +62,7 @@ game.SpendExp = me.ScreenObject.extend({
 				
 			}
 			else if(action === "F5"){
+				//sends user to game screen
 				me.state.change(me.state.PLAY);
 			}
 		});
@@ -69,11 +74,14 @@ game.SpendExp = me.ScreenObject.extend({
 	 *  action to perform when leaving this screen (state change)
 	 */
 	onDestroyEvent: function() {
+		//unregisters f1-f5 keys during game
 		me.input.unbindKey(me.input.KEY.F1, "F1");
 		me.input.unbindKey(me.input.KEY.F2, "F2");
 		me.input.unbindKey(me.input.KEY.F3, "F3");
 		me.input.unbindKey(me.input.KEY.F4, "F4");
 		me.input.unbindKey(me.input.KEY.F5, "F5");
+		//unsubscribes to the handler set up above
+		//basically takes away the values of the keys
 		me.event.unsubscribe(this.handler);
 	}
 });
