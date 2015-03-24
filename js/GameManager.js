d9c9dff5b521ea5ff2b655476afa13d0f361e7c1
Axel Tanner <axeltanner38@gmail.com>
2015-03-24 11:49:46 -0700

    vid 49 comments

--- js/GameManager.js
+++ js/GameManager.js
@@ -226,12 +226,19 @@ game.SpendGold = Object.extend({
 			draw: function(renderer){
 				//draws what you want to write on the screen and the coordinates of it
 				//adding new coords for text
+				//spaces out text by 40s
 				this.font.draw(renderer.getContext(), "PRESS F1-F6 TO BUY, B TO EXIT.  Current Gold: " + game.data.gold, this.pos.x, this.pos.y);
+				//skill 1 text
 				this.font.draw(renderer.getContext(), "Skill 1: Increase Damage. Current Level: " + game.data.skill1 + "Cost: " + ((game.data.skill1+1)*10), this.pos.x, this.pos.y + 40);
+				//skill 2 text
 				this.font.draw(renderer.getContext(), "Skill 2: Run Faster.  Current Level: " + game.data.skill2 + "Cost: " + ((game.data.skill2+1)*10), this.pos.x, this.pos.y + 80);
+				//skill 3 text
 				this.font.draw(renderer.getContext(), "Skill 3: Increase Health.  Current Level: " + game.data.skill3 + "Cost: " + ((game.data.skill3+1)*10), this.pos.x, this.pos.y + 120);
+				//ability 1 text
 				this.font.draw(renderer.getContext(), "Q Ability: Speed Burst.  Current Level: " + game.data.ability1 + "Cost: " + ((game.data.ability1+1)*10), this.pos.x, this.pos.y + 160);
+				//ability 2 text
 				this.font.draw(renderer.getContext(), "W Ability: Eat Your Creep For Health: " + game.data.ability2 + "Cost: " + ((game.data.ability2+1)*10), this.pos.x, this.pos.y + 200);
+				//ability 3 text
 				this.font.draw(renderer.getContext(), "E Ability: Throw Your Spear: " + game.data.ability3 + "Cost: " + ((game.data.ability3+1)*10), this.pos.x, this.pos.y + 240);
 			}
 
