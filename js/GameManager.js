86a861946fa848bd4153cc6e7b58fb58eee5963d
Axel Tanner <axeltanner38@gmail.com>
2015-03-24 12:17:43 -0700

    vid 50 1st

--- js/GameManager.js
+++ js/GameManager.js
@@ -168,6 +168,9 @@ game.SpendGold = Object.extend({
 				this.stopBuying();
 			}
 		}
+
+		this.checkBuyKeys();
+
 		return true;
 	},
 
@@ -255,7 +258,7 @@ game.SpendGold = Object.extend({
 		//returns normal speed to player when unpaused
 		game.data.player.body.setVelocity(game.data.playerMoveSpeed, 20);
 		//removes buy screen when game is unpaused
-		me.game.removeChild(game.data.buyscreen);
+		me.game.world.removeChild(game.data.buyscreen);
 		//unbinding f1-f6 keys
 		me.input.unbindKey(me.input.KEY.F1, "F1", true);
 		me.input.unbindKey(me.input.KEY.F2, "F2", true);
@@ -265,5 +268,90 @@ game.SpendGold = Object.extend({
 		me.input.unbindKey(me.input.KEY.F6, "F6", true);
 		//removes buytext from game
 		me.game.world.removeChild(game.data.buytext);
+	},
+
+	checkBuyKeys: function(){
+		if(me.input.isKeyPressed("F1")){
+			if(this.checkCost(1)){
+				this.makePurchase(1);
+			}
+		}
+		else if(me.input.isKeyPressed("F2")){
+			if(this.checkCost(2)){
+				this.makePurchase(2);
+			}
+		}
+		else if(me.input.isKeyPressed("F3")){
+			if(this.checkCost(3)){
+				this.makePurchase(3);
+			}
+		}
+		else if(me.input.isKeyPressed("F4")){
+			if(this.checkCost(4)){
+				this.makePurchase(4);
+			}
+		}
+		else if(me.input.isKeyPressed("F5")){
+			if(this.checkCost(5)){
+				this.makePurchase(5);
+			}
+		}
+		else if(me.input.isKeyPressed("F6")){
+			if(this.checkCost(6)){
+				this.makePurchase(6);
+			}
+		}
+	},
+
+	checkCost: function(skill){
+		if(skill===1 && (game.data.gold >= ((game.data.skill1+1)*10))){
+			return true;
+		}
+		else if(skill===2 && (game.data.gold >= ((game.data.skill2+1)*10))){
+			return true;
+		}
+		else if(skill===3 && (game.data.gold >= ((game.data.skill3+1)*10))){
+			return true;
+		}
+		else if(skill===4 && (game.data.gold >= ((game.data.ability1+1)*10))){
+			return true;
+		}
+		else if(skill===5 && (game.data.gold >= ((game.data.ability2+1)*10))){
+			return true;
+		}
+		else if(skill===6 && (game.data.gold >= ((game.data.ability3+1)*10))){
+			return true;
+		}
+		else{
+			return false;
+		}
+	},
+
+	makePurchase: function(skill){
+		if(skill === 1){
+			game.data.gold -= ((game.data.skill1 +1)*10);
+			game.data.skill1 += 1;
+			game.data.player.attack += 1;
+		}
+		else if(skill === 2){
+			game.data.gold -= ((game.data.skill2 +1)*10);
+			game.data.skill2 += 1;
+		}
+		else if(skill === 3){
+			game.data.gold -= ((game.data.skill3 +1)*10);
+			game.data.skill3 += 1;
+		}
+		else if(skill === 4){
+			game.data.gold -= ((game.data.ability1 +1)*10);
+			game.data.ability1 += 1;
+		}
+		else if(skill === 5){
+			game.data.gold -= ((game.data.ability2 +1)*10);
+			game.data.ability2 += 1;
+		}
+		else if(skill === 6){
+			game.data.gold -= ((game.data.ability3 +1)*10);
+			game.data.ability3 += 1;
+		}
 	}
 });
